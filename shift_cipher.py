f7e4b8c266075cf78c747f24c2eaa893306f766d
Rahul Noronha <rahulnor@gmail.com>
2021-09-05 07:13:14 +0530

    Created shift cipher enciphering and deciphering functions and tested that they work

---
+++ shift_cipher.py
@@ -0,0 +1,54 @@
+#A shift cipher is a cipher in which each letter in the message is substituted by a letter a few letters down the alphabet.
+#The shift k is an integer value in between 0 and 25.
+
+def encipher(plaintext, key):
+    '''
+    This function takes in a plaintext p, and key k and enciphers the text using the formula given below
+    c = (p+k) mod(26)
+    
+    '''
+    result = ""
+    #Loop used to traverse the characters of the plaintext
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        
+        #If a number or special character is encountered, keep it as it is
+        
+        if(char.isalpha()!=1):
+            result += char
+        # Encrypt uppercase characters
+        elif (char.isupper()):
+            result += chr((ord(char) + key-65) % 26 + 65)
+ 
+        # Encrypt lowercase characters
+        else:
+            result += chr((ord(char) + key - 97) % 26 + 97)
+ 
+    return result
+
+def decipher(ciphertext, key):
+    '''
+    This function takes in a ciphertext c, and key k and deciphers the text using the formula given below
+    p = (c-k) mod(26)
+    
+    '''
+    result = ""
+    #Loop used to traverse the characters of the ciphertext
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        
+        #If a number or special character is encountered, keep it as it is
+        
+        if(char.isalpha()!=1):
+            result += char
+        # Encrypt uppercase characters
+        elif (char.isupper()):
+            result += chr((ord(char) - key-65) % 26 + 65)
+ 
+        # Encrypt lowercase characters
+        else:
+            result += chr((ord(char) - key - 97) % 26 + 97)
+ 
+    return result
+
+    
