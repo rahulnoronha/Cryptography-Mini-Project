f69b903cdc52853af087ea79a1364e22ba575eb7
Rahul Noronha <rahulnor@gmail.com>
2021-10-02 23:16:21 +0530

    Added sripts in Scripts folder

---
+++ Scripts/railfence.py
@@ -0,0 +1,43 @@
+def railfence_encipher(plaintext,depth):
+  matrix = [[] for i in range(depth)]
+  rail  = 0
+  var   = 1
+  for char in plaintext:
+    matrix[rail].append(char)
+    rail += var
+    if rail == depth-1 or rail == 0:
+      var = -var
+  res = ''
+  for i in matrix:
+    for j in i:
+      res += j
+  return res
+
+def railfence_decipher(ciphertext,depth):
+    matrix = [[] for i in range(depth)]
+    rail  = 0
+    var   = 1
+    for char in ciphertext:
+        matrix[rail].append(char)
+        rail += var
+        if rail == depth-1 or rail == 0:
+            var = -var
+    rFence = [[] for i in range(depth)]
+    i = 0
+    l = len(ciphertext)
+    ciphertext = list(ciphertext)
+    for r in matrix:
+        for j in range(len(r)):
+            rFence[i].append(ciphertext[0])
+            ciphertext.remove(ciphertext[0])
+        i += 1
+    rail = 0
+    var  = 1
+    r = ''
+    for i in range(l):
+        r += rFence[rail][0]
+        rFence[rail].remove(rFence[rail][0])
+        rail += var
+        if rail == depth-1 or rail == 0:
+            var = -var
+    return r
